f227b744c21089734a2386d9fb7cba97c588d1d4
vishal4720 <vishalpatole2000@gmail.com>
2024-09-26 20:15:08 +0530

    Updated Assignment 3

--- Assignment 4/Q7/Employee.java
+++ Assignment 4/Q7/Employee.java
@@ -3,13 +3,11 @@ class Employee {
     String name;
     double salary;
 
-    // Constructor to initialize employee details
     Employee(String name, double salary) {
         this.name = name;
         this.salary = salary;
     }
 
-    // Method to display employee details
     void displayDetails() {
         System.out.println("Company: " + companyName + ", Name: " + name + ", Salary: " + salary);
     }
